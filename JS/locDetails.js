860f4ffbed14766abca36edb83dfaa07d923bae2
Mohammad Obeid <mohammadkadoumi77@yahoo.com>
2025-05-05 00:19:08 +0300

    VFinal

--- JS/locDetails.js
+++ JS/locDetails.js
@@ -3,6 +3,16 @@ function back() {
   location.href = "locations.html";
 }
 
+class LocationRes {
+  constructor(moveId, prodId, fromL, code, quantity) {
+    this.moveId = moveId;
+    this.prodId = prodId;
+    this.fromL = fromL;
+    this.code = code;
+    this.quantity = quantity;
+  }
+}
+
 function fetchbalance(id) {
   fetch(`http://localhost:8080/api/v1/product-movement/location/${id}`)
     .then((response) => {
@@ -14,20 +24,42 @@ function fetchbalance(id) {
       return response.json();
     })
     .then((data) => {
+      const raw = localStorage.getItem("selectedLocation");
+      const dat = JSON.parse(raw);
+      console.log(data);
       let i = 0;
       const container = document.getElementById("container");
       container.innerHTML = "";
+
       data.forEach((element) => {
+        const currenLoc = new LocationRes(
+          element.moveId,
+          element.productId,
+          dat.locationName,
+          dat.locationCode,
+          element.quantity
+        );
         const trr = document.createElement("tr");
         i += 1;
         trr.innerHTML = `
     <td>${element.productId}</td>
     <td>${element.productName}</td>
     <td>${element.quantity}</td>
-  `;
+    <td>
+  <button class="btn btn-primary btn-sm" onclick='moveProd(${JSON.stringify(
+    currenLoc
+  )})'>
+    <i class="bi bi-box-arrow-right"></i> Move
+  </button>
+`;
         container.appendChild(trr);
       });
     });
+}
+
+function moveProd(loc) {
+  localStorage.setItem("movedProd", JSON.stringify(loc));
+  location.href = "moveProduct.html";
 }
 
 class Location {
